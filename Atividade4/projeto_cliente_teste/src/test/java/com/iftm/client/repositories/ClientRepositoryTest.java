216a86d624685be29605e345f20788e57b39f5ea
Maria Eduarda <mariaeduarda050501@gmail.com>
2025-05-23 00:09:59 -0300

    ajuste testes

--- Atividade4/projeto_cliente_teste/src/test/java/com/iftm/client/repositories/ClientRepositoryTest.java
+++ Atividade4/projeto_cliente_teste/src/test/java/com/iftm/client/repositories/ClientRepositoryTest.java
@@ -3,6 +3,7 @@ package com.iftm.client.repositories;
 import com.iftm.client.entities.Client;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.Instant;
@@ -14,11 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @DataJpaTest
 public class ClientRepositoryTest {
 
-    private final ClientRepository repositorioCliente;
-
-    public ClientRepositoryTest(ClientRepository repositorioCliente) {
-        this.repositorioCliente = repositorioCliente;
-    }
+    @Autowired
+    private ClientRepository repositorioCliente;
 
     /**
      * Testa a busca de clientes cujo nome contém uma palavra específica, ignorando diferenças de maiúsculas e minúsculas.
@@ -47,15 +45,16 @@ public class ClientRepositoryTest {
     @DisplayName("Verifica a busca de clientes por nome contendo parte de uma string, ignorando diferenças de maiúsculas e minúsculas.")
     void testaBuscaClientesPorNomeContendoParte() {
         // Arrange
-        String palavra = "silvio";
+        String palavra = "on";
 
         // Act
         List<Client> resultado = repositorioCliente.findClientsByNameContaining(palavra);
 
         // Assert
         assertNotNull(resultado);
-        assertEquals(1, resultado.size());
-        assertEquals("Silvio Almeida", resultado.get(0).getName());
+        assertEquals(2, resultado.size());
+        assertEquals("Conceição Evaristo", resultado.get(0).getName());
+        assertEquals("Toni Morrison", resultado.get(1).getName());
     }
 
     /**
@@ -66,8 +65,8 @@ public class ClientRepositoryTest {
     @DisplayName("Verifica a busca de clientes com renda dentro de um intervalo especificado.")
     void testaBuscaClientesPorRendaEntre() {
         // Arrange
-        Double rendaMinima = 2000.0;
-        Double rendaMaxima = 3000.0;
+        Double rendaMinima = 4000.0;
+        Double rendaMaxima = 6000.0;
 
         // Act
         List<Client> resultado = repositorioCliente.findClientsByIncomeBetween(rendaMinima, rendaMaxima);
@@ -75,21 +74,21 @@ public class ClientRepositoryTest {
         // Assert
         assertNotNull(resultado);
         assertEquals(3, resultado.size());
-        assertEquals("Lázaro Ramos", resultado.stream().filter(c -> c.getName().equals("Lázaro Ramos")).findFirst().orElse(null).getName());
-        assertEquals("Gilberto Gil", resultado.stream().filter(c -> c.getName().equals("Gilberto Gil")).findFirst().orElse(null).getName());
-        assertEquals("Jorge Amado", resultado.stream().filter(c -> c.getName().equals("Jorge Amado")).findFirst().orElse(null).getName());
+        assertEquals("Djamila Ribeiro", resultado.stream().filter(c -> c.getName().equals("Djamila Ribeiro")).findFirst().orElse(null).getName());
+        assertEquals("Jose Saramago", resultado.stream().filter(c -> c.getName().equals("Jose Saramago")).findFirst().orElse(null).getName());
+        assertEquals("Silvio Almeida", resultado.stream().filter(c -> c.getName().equals("Silvio Almeida")).findFirst().orElse(null).getName());
     }
 
     /**
-     * Testa a busca de clientes cuja data de nascimento está dentro de um intervalo especificado.
+     * Testa a busca de clientes cuja data de nascimento está num intervalo especificado.
      * Verifica se o resultado contém os clientes esperados.
      */
     @Test
     @DisplayName("Verifica a busca de clientes com data de nascimento dentro de um intervalo especificado.")
     void testaBuscaClientesPorDataNascimentoEntre() {
         // Arrange
-        Instant dataInicio = Instant.parse("1990-01-01T00:00:00Z");
-        Instant dataFim = Instant.parse("2000-01-01T00:00:00Z");
+        Instant dataInicio = Instant.parse("1996-12-23T07:00:00Z");
+        Instant dataFim = Instant.parse("2020-07-12T20:50:00Z");
 
         // Act
         List<Client> resultado = repositorioCliente.findClientsByBirthDateBetween(dataInicio, dataFim);
